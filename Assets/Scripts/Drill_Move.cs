a317af506328469de0c5c689072d1be2cd729665
Nutellaerror404 <ellapohl99@googlemail.com>
2023-06-12 11:37:47 +0200

    Added Obstacles, Implemented Score Implemented drill color change when hitting obstacle

--- Assets/Scripts/Drill_Move.cs
+++ Assets/Scripts/Drill_Move.cs
@@ -1,23 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
 
 public class Drill_Move : MonoBehaviour
 {
     [SerializeField] private Camera camera;
     [SerializeField] private KeyCode Right;
+    [SerializeField] private TMP_Text textUI;
+    private int counter = 0;
     private float moveDirection = -1;
     private float currentMoveDirection;
     private Rigidbody2D RB;
+    private SpriteRenderer Drill;
     public float MoveSpeed;
     public float default_angle;
     public float rotation_speed;
     public float centerOffsetCamera = 4;
+    private int start;
+    private int current;
+    private int score;
+    public int Highscore;
 
     // Start is called before the first frame update
     void Start()
     {
         RB = GetComponent<Rigidbody2D>();
+        Drill = GetComponentInChildren<SpriteRenderer>();
+        start = (int)(transform.position.y+ 0.5f);
+        textUI.text = "Score:" + score;
     }
 
     // Update is called once per frame
@@ -34,7 +47,26 @@ public class Drill_Move : MonoBehaviour
         RB.velocity = (transform.up * -1) * MoveSpeed;  // Drill going locally down
         // RB.velocity = new Vector2(moveDirection, 0)* MoveSpeed;
 
+        // calculate score and set the TextUI
+        current = (int)(transform.position.y + 0.5f);
+        score = start - current;
+        textUI.text = "Score:" + score;
 
         camera.transform.position = new Vector3(0, transform.position.y - centerOffsetCamera, -10); // Camera default following the drill only on y direction
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Obstacle")){
+            counter += 1;
+            if (counter == 1) Drill.color = Color.magenta; 
+            if (counter == 2) Drill.color = Color.red; 
+            Debug.Log("hit Obstacle");
+            Destroy(collision.gameObject);
+            if(counter == 3){
+                SceneManager.LoadScene("Menu");
+                Highscore = score;
+            // set Highscore if it is higher then the old one
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+	float speed = -0.8f;
+	Rigidbody2D myBody;
+
+	// Use this for initialization
+	void Start () {
+		myBody = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x >= 7.3){
+			speed = -speed;
+		} else if(transform.position.x <= -7.4){
+			speed = -speed;
+		}
+		myBody.velocity = new Vector2 (speed, 0);
+
+	}
+}
