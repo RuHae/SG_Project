b4750abfef279327efa313f8d766bb7192dc50f5
Nutellaerror404 <ellapohl99@googlemail.com>
2023-06-29 15:00:18 +0200

    Erdschichten verschwinden nach erscheinen

--- Assets/Scripts/ErdschichteSpawn.cs
+++ Assets/Scripts/ErdschichteSpawn.cs
@@ -7,8 +7,12 @@ public class ErdschichteSpawn : MonoBehaviour
     [SerializeField] GameObject[] erdschicht;
 	[SerializeField] Transform player;
 	List<GameObject> erdschichtenZumErscheinen = new List<GameObject>();
+	GameObject schicht1;
+	GameObject schicht2;
+	GameObject schicht3;
 	public float posi;
 	public float firstSpawn;
+	int counter = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -27,8 +31,23 @@ public class ErdschichteSpawn : MonoBehaviour
 
 	public void newSchicht(float posit)
 	{ // obst in eine queque wenn ein paar drin sind die alten Löschen
-		
-			GameObject obst = Instantiate(erdschicht[Random.Range(0,erdschicht.Length)], this.transform) as GameObject;
-			obst.transform.localPosition = new Vector3(0,posit+1f,0);
+		if(counter == 0 ){
+			schicht1 = Instantiate(erdschicht[Random.Range(0,erdschicht.Length)], this.transform) as GameObject;
+			schicht1.transform.localPosition = new Vector3(0,posit+1f,0);
+			counter = 1;
+			Destroy(schicht2);
+		}else if (counter == 1){
+			schicht2 = Instantiate(erdschicht[Random.Range(0,erdschicht.Length)], this.transform) as GameObject;
+			schicht2.transform.localPosition = new Vector3(0,posit+1f,0);
+			counter = 2;
+			Destroy(schicht3);
+		}else if (counter == 2){
+			schicht3 = Instantiate(erdschicht[Random.Range(0,erdschicht.Length)], this.transform) as GameObject;
+			schicht3.transform.localPosition = new Vector3(0,posit+1f,0);
+			counter = 0;
+			Destroy(schicht1);
+		}
+		// GameObject obst = Instantiate(erdschicht[Random.Range(0,erdschicht.Length)], this.transform) as GameObject;
+		// obst.transform.localPosition = new Vector3(0,posit+1f,0);
 	}
 }
